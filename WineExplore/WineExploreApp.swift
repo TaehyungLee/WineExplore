2335c7ee3116c6763bd6113be491e0b3b89b76bf
LeeTaehyung <thlee@kb-sys.com>
2022-09-13 16:04:41 +0900

    Initial Commit

---
+++ WineExplore/WineExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  WineExploreApp.swift
+//  WineExplore
+//
+//  Created by Taehyung Lee on 2022/09/13.
+//
+
+import SwiftUI
+
+@main
+struct WineExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
